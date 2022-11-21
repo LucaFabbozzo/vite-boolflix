0071524753d8fcb7822925c94daffdf075bd0a66
LucaFabbozzo <lucafabbozzo@gmail.com>
2022-11-21 05:55:33 -0500

    [add] first api call whit mounted

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,28 @@
   <script>
+import {store} from './data/store'
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 import AppCard from './components/AppCard.vue';
+import axios from 'axios';
   export default {
   name: 'App',
-  components: {AppHeader, AppMain, AppCard}
+  components: { AppHeader, AppMain, AppCard },
+  data() {
+    return {
+      store
+    }
+  },
+  mounted() {
+    axios.get(store.apiUrl)
+      .then(result => {
+        console.log(result.data.results)
+      })
+      .catch(error => {
+      console.log(error)
+    })
   }
+}
+
   </script>
 
 <template>
---
+++ src/data/store.js
@@ -0,0 +1,5 @@
+import { reactive } from 'vue'
+
+export const store = reactive({
+  apiUrl: "https://api.themoviedb.org/3/search/movie?api_key=b47aa60d77a4dbf3638e882b64e4c97a&query=matrix",
+});
